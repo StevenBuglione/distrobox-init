c844a88ab58f8df91e65f7f85a964dadda5a9da9
sbuglione <stevenbuglione1@gmail.com>
2024-11-17 22:46:19 -0500

    Initial Commit

--- distro/fedora/config.sh
+++ distro/fedora/config.sh
@@ -12,12 +12,20 @@ echo "Sourcing Nix..."
 echo "Removing Previous Install Of Home-Manager..."
 rm -f /home/sbuglione/.local/state/nix/profiles/home-manager*
 rm -f /home/sbuglione/.local/state/home-manager/gcroots/current-home
-
+rm -r /home/sbuglione/.config/home-manager/
 
 echo "Installing Home Manager..."
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
 nix-shell '<home-manager>' -A install
+
+echo "Removing Previous Install Of Home-Manager Repo..."
+rm -r /home/.config/home-manager/
+nix-shell -p git --run "git clone https://github.com/StevenBuglione/home-manager.git /home/.config/home-manager"
+
+echo "Running Home-Manager..."
+home-manager switch
+
 
 exit 0
