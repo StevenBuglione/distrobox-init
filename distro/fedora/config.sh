cfaf408e818979b5d253a49082cdba781dd57cdb
sbuglione <stevenbuglione1@gmail.com>
2024-11-17 23:15:01 -0500

    Initial Commit

--- distro/fedora/config.sh
+++ distro/fedora/config.sh
@@ -24,9 +24,10 @@ rm -r /home/sbuglione/.oh-my-zsh/
 
 echo "Installing Oh-My-Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm /home/sbuglione/.zshrc
 
 echo "Removing Previous Install Of Home-Manager Repo..."
-rm -r /home/sbuglione/.config/home-manager/
+sudo rm -r /home/sbuglione/.config/home-manager/
 nix-shell -p git --run "git clone https://github.com/StevenBuglione/home-manager.git /home/sbuglione/.config/home-manager"
 
 echo "Running Home-Manager..."
@@ -34,5 +35,7 @@ home-manager switch
 
 echo "Setting Zsh As Default Shell..."
 chsh -s $(which zsh)
+
+source /home/sbuglione/.zshrc
 
 exit 0
