66b665d5c5ead205ec2cf247affdb7ec9a143f7e
sbuglione <stevenbuglione1@gmail.com>
2024-11-17 21:47:31 -0500

    Initial Commit

--- distro/fedora/config.sh
+++ distro/fedora/config.sh
@@ -3,11 +3,14 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 
 echo "Updating Nix Container Ownership..."
 sudo chown -R $(whoami):$(whoami) /nix/;
+sudo chmod +x -R /nix/var/nix/profiles/default/etc/profile.d/
+
+echo "Sourcing Nix..."
+source ~/.bashrc;
 
 echo "Installing Home Manager..."
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager;
 sudo nix-channel --update;
 
-source ~/.bashrc;
 nix-shell '<home-manager>' -A install;
 
