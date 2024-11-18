d3db3536fb8201cffda091fe33705628ba92c190
sbuglione <stevenbuglione1@gmail.com>
2024-11-17 22:01:38 -0500

    Initial Commit

--- distro/fedora/config.sh
+++ distro/fedora/config.sh
@@ -1,16 +1,25 @@
 echo "Installing Nix..."
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm;
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 
 echo "Updating Nix Container Ownership..."
-sudo chown -R $(whoami):$(whoami) /nix/;
+sudo chown -R $(whoami):$(whoami) /nix
 sudo chmod +x -R /nix/var/nix/profiles/default/etc/profile.d/
 
 echo "Sourcing Nix..."
-source ~/.bashrc;
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+# Check and remove the files if they exist
+if [ -e /home/$(whoami)/.local/state/nix/profiles/home-manager* ]; then
+  rm -fv /home/$(whoami)/.local/state/nix/profiles/home-manager*
+fi
+
+if [ -e /home/$(whoami)/.local/state/home-manager/gcroots/current-home ]; then
+  rm -fv /home/$(whoami)/.local/state/home-manager/gcroots/current-home
+fi
 
 echo "Installing Home Manager..."
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager;
-sudo nix-channel --update;
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
 
-nix-shell '<home-manager>' -A install;
+nix-shell '<home-manager>' -A install
 
