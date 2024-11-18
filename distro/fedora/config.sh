25b75ef88a3239fb791fc751043111d7055b0a00
sbuglione <stevenbuglione1@gmail.com>
2024-11-17 22:31:35 -0500

    Initial Commit

--- distro/fedora/config.sh
+++ distro/fedora/config.sh
@@ -2,15 +2,15 @@ echo "Installing Nix..."
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 
 echo "Updating Nix Container Ownership..."
-sudo chown -R $(whoami):$(whoami) /nix
+sudo chown -R sbuglione:sbuglione /nix
 sudo chmod +x -R /nix/var/nix/profiles/default/etc/profile.d/
 
 echo "Sourcing Nix..."
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 echo "Removing Previous Install Of Home-Manager..."
-rm -f /home/$(whoami)/.local/state/nix/profiles/home-manager*
-rm -f /home/$(whoami)/.local/state/home-manager/gcroots/current-home
+rm -f /home/sbuglione/.local/state/nix/profiles/home-manager*
+rm -f /home/sbuglione/.local/state/home-manager/gcroots/current-home
 
 
 echo "Installing Home Manager..."
