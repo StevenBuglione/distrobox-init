973876c94a37bf489d166e9fbea21e204b101607
sbuglione <stevenbuglione1@gmail.com>
2024-11-17 22:16:42 -0500

    Initial Commit

--- distro/fedora/config.sh
+++ distro/fedora/config.sh
@@ -8,14 +8,10 @@ sudo chmod +x -R /nix/var/nix/profiles/default/etc/profile.d/
 echo "Sourcing Nix..."
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
-# Check and remove the files if they exist
-if [ -e /home/$(whoami)/.local/state/nix/profiles/home-manager* ]; then
-  rm -fv /home/$(whoami)/.local/state/nix/profiles/home-manager*
-fi
+echo "Removing Previous Install Of Home-Manager..."
+rm -f /home/$(whoami)/.local/state/nix/profiles/home-manager*
+rm -f /home/$(whoami)/.local/state/home-manager/gcroots/current-home
 
-if [ -e /home/$(whoami)/.local/state/home-manager/gcroots/current-home ]; then
-  rm -fv /home/$(whoami)/.local/state/home-manager/gcroots/current-home
-fi
 
 echo "Installing Home Manager..."
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
